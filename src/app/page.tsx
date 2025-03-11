b07abd30543e5001b1cc5f8d1b5efa926eebb9e7
Robert Elliott <robert.elliott2925@gmail.com>
2025-03-10 23:01:41 -0600

    Added a few comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import Hero from "@/components/Hero/hero";
 import Menu from "@/components/Menu/menu";
 
 export default function Home() {
-
   return (
     <div className={styles.page}>
-      <Hero />
+      <Hero /> {/* Temporary video placeholder, waiting input from sponsor */}
       <div id="keokuk" className={styles.about_section}>
-        <Menu />
+        <Menu /> {/* About section for Keokuk. Waiting input from sponsor */}
       </div>
     </div>
   );
